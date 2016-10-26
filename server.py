d3d75231495ad88b118b51ff5c09c0e79959e314
a.goscinski@mail.tu-berlin.de <a.goscinski@mail.tu-berlin.de>
2016-10-26 15:34:32 +0800

    added Marketsimulation initialization

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ import quickfix as fix
 import sys
 import time
 import pdb
+import yahoo_finance
+import MySQLdb
 from enum import Enum
 
 
@@ -134,8 +136,12 @@ class ServerLogic:
         self.server_fix_handler = ServerFIXHandler(self, server_config_file_name)
         self.server_database_handler = ServerDatabaseHandler()
         self.market_simulation_handler = MarketSimulationHandler()
+        self.initialize_new_stocks = True
 
     def start_server(self):
+        if self.initialize_new_stocks:
+            self.server_database_handler.delete_all_stock_data()
+            self.market_simulation_handler.init()
         self.server_fix_handler.start()
         while 1: time.sleep(1)
         self.stop_server()
@@ -174,11 +180,44 @@ class ServerLogic:
         # TODO #29 add authentication
         return ServerRespond.AUTHENTICATION_SUCCESS
 
-
 class ServerDatabaseHandler:
     # TODO send SQL Queries
     def __init__(self):
         pass
+
+    def delete_all_stock_data(self):
+        try:
+            conn = MySQLdb.connect(host='localhost', user='root', passwd='root', db='FSCDatabase', port=3306)
+            cur = conn.cursor()
+            execution = ("delete from Stock")
+            cur.execute(execution)
+            conn.commit()
+            conn.close()
+        except MySQLdb.Error, e:
+            print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+
+
+    def delete_stock_data(self,stock):
+        try:
+            conn = MySQLdb.connect(host='localhost', user='root', passwd='root', db='FSCDatabase', port=3306)
+            cur = conn.cursor()
+            execution = ("delete from Stock where Ticker = '%s' limit 1" % stock.ticker)
+            cur.execute(execution)
+            conn.commit()
+            conn.close()
+        except MySQLdb.Error, e:
+            print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+
+    def insert_stock_data(self, stock):
+        try:
+            conn = MySQLdb.connect(host='localhost', user='root', passwd='root', db='FSCDatabase', port=3306)
+            cur = conn.cursor()
+            execution = ("insert into Stock(Ticker, CompanyName, CurrentPrice, CurrentVolume) values( '%s', '%s', '%s', '%s')" % (stock.ticker, stock.company_name, stock.current_price, stock.current_volume))
+            cur.execute(execution)
+            conn.commit()
+            conn.close()
+        except MySQLdb.Error, e:
+            print "Mysql Error %d: %s" % (e.args[0], e.args[1])
 
     def send_client_match_query(self):
         pass
@@ -198,26 +237,59 @@ class ServerDatabaseHandler:
 
 
 class MarketSimulationHandler:
-    stock_market = "NYSE"
 
-    # TODO
-    def request_market_data(self, symbols):
-        """Request market data for initialization
+    def __init__(self):
+        self.stock_list_file_name = "stock_list.cfg"
+        self.stock_list = read_file_values(self.stock_list_file_name)
 
-		Used for server initialization to fetch data
+    def init(self):
+        self.load_market_data_into_database()
 
-		Args:
-		    symbols (list of str): A list of symbol tickers.
+    def load_market_data_into_database(self):
+        """Loads market data for initialization
+
+		Used for server initialization to fetch data from a finance server
 
 		Returns:
-		    bool: The return value. True for success, False otherwise.
+		    None
 		"""
-        pass
+        for stock in self.stock_list:
+            share = yahoo_finance.Share(stock)
+            new_stock = Stock(share.data_set["symbol"], share.data_set["Name"], share.data_set["LastTradePriceOnly"], share.data_set["Volume"])
+            ServerDatabaseHandler().insert_stock_data(new_stock)
 
 
-try:
-    file = sys.argv[1] if len(sys.argv) == 2 else "server.cfg"
-    server = ServerLogic(file)
-    server.start_server()
-except fix.ConfigError, e:
-    print e
+class Stock:
+
+    def __init__(self, ticker, company_name=None, current_price=None, current_volume=None):
+        self.ticker = ticker
+        self.company_name = company_name
+        self.current_price = current_price
+        self.current_volume = current_volume
+
+    def get_ticker(self):
+        return self.ticker
+
+    def get_company_name(self):
+        return self.company_name
+
+    def get_current_price(self):
+        return self.current_price
+
+    def get_current_volume(self):
+        return self.current_volume
+
+
+
+def read_file_values(file_name):
+    """Produces a list out of a file, each line represents one value
+
+    Args:
+        file_name (string): name of the file
+
+    Returns:
+        list (string): A list of stock names
+    """
+    with open(file_name, 'r') as file:
+        list = file.read().splitlines()
+    return list
---
+++ test_server.py
@@ -0,0 +1,21 @@
+import server
+
+nasdaq_stock_ticker = "TESTTICKER"
+nasdaq_stock = server.Stock(nasdaq_stock_ticker)
+
+def setup_module(module):
+    """ setup any state specific to the execution of the given module."""
+
+
+def teardown_module(module):
+    """ teardown any state that was previously setup with a setup_module
+    method.
+    """
+    server.ServerDatabaseHandler().delete_stock_data(nasdaq_stock)
+
+class TestMarketSimulator:
+    market_simulation_handler = server.MarketSimulationHandler()
+    market_simulation_handler.stock_list = [nasdaq_stock_ticker]
+
+    def test_load_market_data(self):
+        self.market_simulation_handler.load_market_data()
