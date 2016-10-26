ea4da4c7969bf7d763e80e084411b66de23af7e9
a.goscinski@mail.tu-berlin.de <a.goscinski@mail.tu-berlin.de>
2016-10-26 15:43:04 +0800

    created main file; fixed method name in server.py

--- test_server.py
+++ test_server.py
@@ -17,5 +17,5 @@ class TestMarketSimulator:
     market_simulation_handler = server.MarketSimulationHandler()
     market_simulation_handler.stock_list = [nasdaq_stock_ticker]
 
-    def test_load_market_data(self):
-        self.market_simulation_handler.load_market_data()
+    def test_load_market_data_into_database(self):
+        self.market_simulation_handler.load_market_data_into_database()
