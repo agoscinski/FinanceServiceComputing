8170468dc5ceb6a6b1e565bcfd3304dd394b7ded
a.goscinski@mail.tu-berlin.de <a.goscinski@mail.tu-berlin.de>
2016-11-30 19:08:03 +0800

    changed test database inserts path

--- tests/database/init_test_database.sql
+++ tests/database/init_test_database.sql
@@ -1,9 +1,9 @@
 -- create structure and tables
 source tests/database/create_tables.sql
 -- create views
-source tests/database/create_views.sql
+source database/create_views.sql
 -- insert data
-source tests/database/account_inserts.sql
-source tests/database/stock_inserts.sql
-source tests/database/order_inserts.sql
-source tests/database/order_execution_inserts.sql
+source database/account_inserts.sql
+source database/stock_inserts.sql
+source database/order_inserts.sql
+source database/order_execution_inserts.sql
