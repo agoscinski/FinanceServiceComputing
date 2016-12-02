0932178557a44f984d37def39251f341a62aec5c
rentseen <yelinmgl@hotmail.com>
2016-12-02 14:05:01 +0800

    add variables to client configure script

--- client_cfg_script.py
+++ client_cfg_script.py
@@ -5,9 +5,38 @@ import MySQLdb
 
 class CreateClientCFG():
 
-    def __init__(self, file_name="test_client.cfg", application_id="client"):
+    def __init__(self, file_name="test_client.cfg", application_id="client", connection_type="initiator",
+                    file_store_path="storage/client_messages", file_log_path="log/client_log", start_time="00:01:00",
+                    end_time="23:59:00", heart_bt_int="30",
+                    socket_accept_port="5520", use_data_dictionary="Y", data_dictionary="spec/FIX42.xml",
+                    validate_user_defined_fields="N", validate_incoming_message="N",
+                    refresh_on_logon="Y", reset_on_logon="Y", reset_on_logout="Y",
+                    http_accept_port="9911", reconnect_interval="60", begin_string="FIX.4.2",
+                    target_comp_id="server", socket_connect_host="localhost",
+                    socket_connect_port="5501" ):
         self.file_name = file_name
         self.application_id = application_id
+        self.connection_type = connection_type
+        self.file_store_path = file_store_path
+        self.file_log_path = file_log_path
+        self.start_time = start_time
+        self.end_time = end_time
+        self.heart_bt_int = heart_bt_int
+        self.socket_accept_port = socket_accept_port
+        self.use_data_dictionary = use_data_dictionary
+        self.data_dictionary = data_dictionary
+        self.validate_user_defined_fields = validate_user_defined_fields
+        self.validate_incoming_message = validate_incoming_message
+        self.refresh_on_logon = refresh_on_logon
+        self.reset_on_logon = reset_on_logon
+        self.reset_on_logout = reset_on_logout
+        self.http_accept_port = http_accept_port
+        self.reconnect_interval = reconnect_interval
+
+        self.begin_string = begin_string
+        self.target_comp_id = target_comp_id
+        self.socket_connect_host = socket_connect_host
+        self.socket_connect_port = socket_connect_port
         #TODO the rest attributes
 
     def write_script(self):
@@ -22,34 +51,33 @@ class CreateClientCFG():
                 outString="# copy of http://stackoverflow.com/questions/35983928/how-to-send-fix-message-with-quickfix-j\
                             \n[DEFAULT]\
                             \nApplicationID="+self.application_id+"\
-                            \nConnectionType=initiator\
-                            \nFileStorePath=storage/client_messages\
-                            \nFileLogPath=log/client_log\
-                            \nStartTime=00:01:00\
-                            \nEndTime=23:59:00\
-                            \nHeartBtInt=30\
-                            \nSocketAcceptPort=5520\
-                            \nUseDataDictionary=Y\
-                            \nDataDictionary=spec/FIX42.xml\
-                            \nValidateUserDefinedFields=N\
-                            \nValidateIncomingMessage=N\
-                            \nRefreshOnLogon=Y\
-                            \nValidateUserDefinedFields=N\
-                            \nResetOnLogon=Y\
-                            \n#ResetOnLogout=Y\
-                            \n#HttpAcceptPort=9911\
-                            \n#ReconnectInterval=60\
+                            \nConnectionType="+self.connection_type+"\
+                            \nFileStorePath="+self.file_store_path+"\
+                            \nFileLogPath="+self.file_log_path+"\
+                            \nStartTime="+self.start_time+"\
+                            \nEndTime="+self.end_time+"\
+                            \nHeartBtInt="+self.heart_bt_int+"\
+                            \nSocketAcceptPort="+self.socket_accept_port+"\
+                            \nUseDataDictionary="+self.use_data_dictionary+"\
+                            \nDataDictionary="+self.data_dictionary+"\
+                            \nValidateUserDefinedFields="+self.validate_user_defined_fields+"\
+                            \nValidateIncomingMessage="+self.validate_incoming_message+"\
+                            \nRefreshOnLogon="+self.refresh_on_logon+"\
+                            \nResetOnLogon="+self.reset_on_logon+"\
+                            \n#ResetOnLogout="+self.reset_on_logout+"\
+                            \n#HttpAcceptPort="+self.http_accept_port+"\
+                            \n#ReconnectInterval="+self.reconnect_interval+"\
                             \n\n\n# standard config elements"
                 for row in results:
                     outString=outString+"\n\n[SESSION]\
                                             \n# inherit ConnectionType, ReconnectInterval and SenderCompID from default\
-                                            \nBeginString=FIX.4.2\
+                                            \nBeginString="+self.begin_string+"\
                                             \nSenderCompID="+row[0]+"\
-                                            \nTargetCompID=server\
-                                            \nSocketConnectHost=localhost\
-                                            \nSocketConnectPort=5501"
+                                            \nTargetCompID="+self.target_comp_id+"\
+                                            \nSocketConnectHost="+self.socket_connect_host+"\
+                                            \nSocketConnectPort="+self.socket_connect_port+""
 
-                    client_cfg.write(outString)
+                client_cfg.write(outString)
             except MySQLdb.Error,e:
                 print "Mysql Error %d: %s" % (e.args[0], e.args[1])
 
